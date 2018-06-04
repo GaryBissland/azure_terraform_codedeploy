850b4613909e1dd399d4d5aee7bed7ba539c7bbc
Gary Bissland <gary_bissland@hotmail.com>
2018-06-04 16:33:01 +0100

    update script

--- sample_app/updatefile.sh
+++ sample_app/updatefile.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "My new file 456" > /home/azureuser/sampletxtfile2.txt
+container=$(< scn.txt)
+echo $container
+account=$(< san.txt)
+sas_token=$(cat sas_token.json | jq -r .serviceSasToken)
+
+az storage blob download --container-name $container --file "./updateapp.jar" --name "updateapp.jar" --account-name $account --sas-token $sas_token
+sudo java -jar -Dserver.port=8080 updateapp.jar
