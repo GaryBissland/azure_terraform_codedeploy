af2a21554b2b4a3e1a525791709eee3948e99774
Gary Bissland <gary_bissland@hotmail.com>
2018-06-05 12:34:53 +0100

    debug

--- sample_app/updatefile.sh
+++ sample_app/updatefile.sh
@@ -2,9 +2,9 @@
 
 cd /home/azureuser
 container=$(< scn.txt)
-echo $container < ct.txt
+echo $container >> ct.txt
 account=$(< san.txt)
 sas_token=$(cat sas_token.json | jq -r .serviceSasToken)
 
-az storage blob download --container-name $container --file "./updateapp.jar" --name "updateapp.jar" --account-name $account --sas-token $sas_token
+az storage blob download --container-name "$container" --file "./updateapp.jar" --name "updateapp.jar" --account-name "$account" --sas-token "$sas_token"
 sudo java -jar -Dserver.port=8080 updateapp.jar
