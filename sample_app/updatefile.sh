e94c91b220c0523face11d6945ba264578556488
Gary Bissland <gary_bissland@hotmail.com>
2018-06-05 16:00:18 +0100

    Auto stash before merge of "master" and "origin/master"
    kill running springboot app

--- sample_app/updatefile.sh
+++ sample_app/updatefile.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cd /home/azureuser
+process=$(cat "pid.file")
+kill $process
 container=$(cat "scn.txt")
-echo $container >> ct.txt
 account=$(cat "san.txt")
 sas_token=$(cat "sas_token.json" | jq -r .serviceSasToken)
 
 az storage blob download --container-name "$container" --file "./updateapp.jar" --name "updateapp.jar" --account-name "$account" --sas-token "$sas_token"
-sudo java -jar -Dserver.port=8080 updateapp.jar
+sudo java -jar -Dserver.port=8080 updateapp.jar & echo $! >> ./pid.file &
