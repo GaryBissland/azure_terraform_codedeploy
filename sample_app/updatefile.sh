0cbb2b82da1fcca8e1d2e44d052e8e5b4e66e1ea
Gary Bissland <gary_bissland@hotmail.com>
2018-06-05 16:25:22 +0100

    Update updatefile.sh

--- sample_app/updatefile.sh
+++ sample_app/updatefile.sh
@@ -8,4 +8,4 @@ account=$(cat "san.txt")
 sas_token=$(cat "sas_token.json" | jq -r .serviceSasToken)
 
 az storage blob download --container-name "$container" --file "./updateapp.jar" --name "updateapp.jar" --account-name "$account" --sas-token "$sas_token"
-sudo java -jar -Dserver.port=8080 updateapp.jar & echo $! >> ./pid.file &
+sudo java -jar -Dserver.port=8080 updateapp.jar & echo $! | sudo tee ./pid.file &
